010e9c3676e6ed964471d32c15e1fa56d25f5bd0
Kirill Varikov <35407446+CyrilVarikov@users.noreply.github.com>
2018-02-22 13:07:07 +0300

    Implemet third task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
-    // your implementation
+     this.arr = [];
+     this.compare;
+     this.hardCompare = 0;
+
   }
 
   add(element) {
-    // your implementation
+    return this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
+
+
   sort(indices) {
-    // your implementation
+    var tempArr = [];
+    indices.sort(function (a, b) {
+      return a - b;
+    });
+    if (indices.length !== 1) {
+      for (var i = 0; i < indices.length; i++) {
+        tempArr.push(this.arr[indices[i]]);
+      }
+      if (this.hardCompare === 1) {
+        tempArr.sort(this.compare);
+      } else {
+        tempArr.sort(function (a, b) {
+          return a - b;
+        });
+      }
+      for (var j = 0; j < tempArr.length; j++) {
+        this.arr.splice(indices[j], 1, tempArr[j]);
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compare = compareFunction;
+    this.hardCompare = 1;
   }
 }
 
